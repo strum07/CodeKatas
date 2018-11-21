2a6434ceb90f2440051a96e283c264a688d2e29e
Sagar Sudhakar <sagarsudhakar@live.com>
2018-11-20 23:40:48 -0600

    Refactored Pivot Finder to O(LogN) from O(N)

--- src/main/java/com/sagarsudhakar/PivotFinder/PivotFinder.java
+++ src/main/java/com/sagarsudhakar/PivotFinder/PivotFinder.java
@@ -3,17 +3,35 @@ package com.sagarsudhakar.PivotFinder;
 public class PivotFinder {
 
 
-    public static int findPivot(int[] arr) {
+    public static int findPivot(int[] inputArray) {
         int pivot = -1;
 
-        if(arr.length<3)
+        if(inputArray.length<3)
             return pivot;
 
-        for(int i=1; i<arr.length-1;i++){
-                if(arr[i]>=arr[i-1] && arr[i]>arr[i+1])
+        for(int i=1; i<inputArray.length-1;i++){
+                if(inputArray[i]>inputArray[i-1] && inputArray[i]>inputArray[i+1])
                     return i;
         }
 
         return pivot;
     }
+
+    public static int findPivotBinarySearch(int[] inputArray , int start, int end) {
+        int pivot = -1;
+
+        if((end-start<3) || inputArray.length<3)
+            return pivot;
+
+        int mid = start+((end-start)/2);
+
+        if(inputArray[mid]>inputArray[mid-1]&&inputArray[mid]>inputArray[mid+1])
+            return mid;
+
+        if(inputArray[mid]<inputArray[mid+1])
+            return findPivotBinarySearch(inputArray, mid+1, end);
+
+        return findPivotBinarySearch(inputArray,start,mid-1);
+    }
+
 }
--- src/test/java/com/sagarsudhakar/PivotFinderTests/PivotFinderTests.java
+++ src/test/java/com/sagarsudhakar/PivotFinderTests/PivotFinderTests.java
@@ -20,28 +20,28 @@ public class PivotFinderTests {
 
 
     @Test
-    public void findPivot_EmptyArray_ReturnsMinus1(){
+    public void findPivot_EmptyArray_ReturnsInvalid(){
         int[] arr = {};
         int pivotIndex = PivotFinder.findPivot(arr);
         Assert.assertEquals(-1,pivotIndex);
     }
 
     @Test
-    public void findPivot_SameElements_ReturnsMinus1(){
+    public void findPivot_SameElements_ReturnsInvalid(){
         int[] arr = {1,1,1,1,1};
         int pivotIndex = PivotFinder.findPivot(arr);
         Assert.assertEquals(-1,pivotIndex);
     }
 
     @Test
-    public void findPivot_IncreasingOrder_ReturnsMinus1(){
+    public void findPivot_IncreasingOrder_ReturnsInvalid(){
         int[] arr = {1,2,3,4,5};
         int pivotIndex = PivotFinder.findPivot(arr);
         Assert.assertEquals(-1,pivotIndex);
     }
 
     @Test
-    public void findPivot_DecreasingOrder_ReturnsMinus1(){
+    public void findPivot_DecreasingOrder_ReturnsInvalid(){
         int[] arr = {4,3,2,1};
         int pivotIndex = PivotFinder.findPivot(arr);
         Assert.assertEquals(-1,pivotIndex);
@@ -55,9 +55,52 @@ public class PivotFinderTests {
     }
 
     @Test
-    public void findPivot_MultiplePivotPresent_ReturnsIndexOfLatestPivot(){
+    public void findPivot_MultiplePivotPresent_ReturnsInvalid(){
         int[] arr = {1,2,3,4,4,3,2,1};
         int pivotIndex = PivotFinder.findPivot(arr);
-        Assert.assertEquals(4,pivotIndex);
+        Assert.assertEquals(-1,pivotIndex);
+    }
+
+
+    @Test
+    public void findPivotBinary_EmptyArray_ReturnsInvalid(){
+        int[] arr = {};
+        int pivotIndex = PivotFinder.findPivotBinarySearch(arr,0,arr.length-1);
+        Assert.assertEquals(-1,pivotIndex);
+    }
+
+    @Test
+    public void findPivotBinary_SameElements_ReturnsInvalid(){
+        int[] arr = {1,1,1,1,1};
+        int pivotIndex = PivotFinder.findPivotBinarySearch(arr,0,arr.length-1);
+        Assert.assertEquals(-1,pivotIndex);
+    }
+
+    @Test
+    public void findPivotBinary_IncreasingOrder_ReturnsInvalid(){
+        int[] arr = {1,2,3,4,5};
+        int pivotIndex = PivotFinder.findPivotBinarySearch(arr,0,arr.length-1);
+        Assert.assertEquals(-1,pivotIndex);
+    }
+
+    @Test
+    public void findPivotBinary_DecreasingOrder_ReturnsInvalid(){
+        int[] arr = {4,3,2,1};
+        int pivotIndex = PivotFinder.findPivotBinarySearch(arr,0,arr.length-1);
+        Assert.assertEquals(-1,pivotIndex);
+    }
+
+    @Test
+    public void findPivotBinary_OnePivotPresent_ReturnsIndexOfPivot(){
+        int[] arr = {1,2,3,4,3,2,1};
+        int pivotIndex = PivotFinder.findPivotBinarySearch(arr,0,arr.length-1);
+        Assert.assertEquals(3,pivotIndex);
+    }
+
+    @Test
+    public void findPivotBinary_MultiplePivotPresent_ReturnsInvalid(){
+        int[] arr = {1,2,3,4,4,3,2,1};
+        int pivotIndex = PivotFinder.findPivotBinarySearch(arr,0,arr.length-1);
+        Assert.assertEquals(-1,pivotIndex);
     }
 }
